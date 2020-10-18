39372e5d8986c7694bf7cb3f2cb06d81dfb08779
Mks <maxnxi@icloud.com>
2020-10-18 05:50:57 +0300

    Day 1

---
+++ Chessman/Controller/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  Chessman
+//
+//  Created by Maksim Ponomarev on 18.10.2020.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
---
+++ Chessman/Utilities/Chessman.swift
@@ -0,0 +1,17 @@
+//
+//  Chessman.swift
+//  Chessman
+//
+//  Created by Maksim Ponomarev on 18.10.2020.
+//
+
+import Foundation
+
+class Chessman {
+    
+    enum ChessmanColor {
+        case black
+        case white
+    }
+    
+}
