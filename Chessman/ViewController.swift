c4dae4e541fce3078866b49f1bceecd4aa5d68d3
Mks <maxnxi@icloud.com>
2020-10-18 05:07:59 +0300

    Begin

---
+++ Chessman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chessman
+//
+//  Created by Maksim Ponomarev on 18.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
